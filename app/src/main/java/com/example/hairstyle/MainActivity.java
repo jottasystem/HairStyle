90c4d8d6764599aef9440432b45ad29ecf814907
João Paulo de Sousa <opah@MacBook-Opah.local>
2019-04-17 19:15:16 -0300

    Navegation

--- app/src/main/java/com/example/hairstyle/MainActivity.java
+++ app/src/main/java/com/example/hairstyle/MainActivity.java
@@ -5,24 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonLogin;
+
+    TextView textRegisterClick;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         buttonLogin = findViewById(R.id.idButtonLogin);
-
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),SideBar.class);
-
                 startActivity(intent);
             }
         });
+
+        textRegisterClick = findViewById(R.id.idTextRegister);
+        textRegisterClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Register.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
