fcec161819583bf327ad83b5084e5a09f05c66b5
João Paulo de Sousa <opah@MacBook-Opah.local>
2019-04-17 03:20:57 -0300

    Sidebar Drawer

--- app/src/main/java/com/example/hairstyle/MainActivity.java
+++ app/src/main/java/com/example/hairstyle/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.hairstyle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLogin = findViewById(R.id.idButtonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SideBar.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
