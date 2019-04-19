11afaebda6b99aeae2372948530de4da143481df
João Paulo de Sousa <opah@MacBook-Opah.local>
2019-04-19 14:04:38 -0300

    new screens

--- app/src/main/java/com/example/hairstyle/ForgotPassword.java
+++ app/src/main/java/com/example/hairstyle/ForgotPassword.java
@@ -1,16 +1,28 @@
 package com.example.hairstyle;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ForgotPassword extends AppCompatActivity {
-
+private Button buttonNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Esqueci minha senha");
+
+        buttonNext = findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),NewPassword.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hairstyle/NewPassword.java
@@ -0,0 +1,14 @@
+package com.example.hairstyle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewPassword extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_password);
+
+    }
+}
